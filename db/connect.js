6dbeef776a443ee3dbbcc307722eb0fad0d12f2d
do-Va <varisdogukan@outlook.com>
2022-06-19 11:46:18 +0300

    Connected database

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,12 @@
-const connectionString =
-  'mongodb+srv://dova:<password>@nodeexpressprojects.g20gapc.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+
+const connectDB = url => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
