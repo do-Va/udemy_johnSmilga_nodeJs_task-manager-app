906d9db1502eb49d51ff44d509ae3477c195551c
do-Va <varisdogukan@outlook.com>
2022-06-20 21:56:19 +0300

    Updated getTask conroller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -20,8 +20,20 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    // Task koleksiyonun içerisinde paramsdan gelen id ile aynı id'ye sahip elemanı bul.
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
