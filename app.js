85ee98af35c52ef4ccbd4868e7ab50c550425db9
do-Va <varisdogukan@outlook.com>
2022-06-20 23:35:04 +0300

    Added 404 notFound middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
 require('dotenv').config();
 
 // middleware
@@ -11,11 +12,7 @@ app.use(express.json());
 // routes
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks')             - get all the tasks
-// app.post('/api/v1/tasks')            - create a new task
-// app.get('/api/v1/tasks/:id')         - get single task
-// app.patch('/api/v1/tasks/:id')       - update task
-// app.delete('/api/v1/tasks/:id')      - delete task
+app.use(notFound);
 
 const port = 3000;
 
