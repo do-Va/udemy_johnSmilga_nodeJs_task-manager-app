fd4f07644e5769bd8d2679f6fc8e8728d51addd9
do-Va <varisdogukan@outlook.com>
2022-06-20 22:18:59 +0300

    Updated deleteTask controller.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -22,8 +22,9 @@ const createTask = async (req, res) => {
 
 const getTask = async (req, res) => {
   try {
-    // Task koleksiyonun içerisinde paramsdan gelen id ile aynı id'ye sahip elemanı bul.
     const { id: taskID } = req.params;
+
+    // Task koleksiyonun içerisinde paramsdan gelen id ile aynı id'ye sahip elemanı bul.
     const task = await Task.findOne({ _id: taskID });
 
     if (!task) {
@@ -40,8 +41,21 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    // Silinecek elemanı bulur ve sildikten sonra o elemanı geri döner.
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
