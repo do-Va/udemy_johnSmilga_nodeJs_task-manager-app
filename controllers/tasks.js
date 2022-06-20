32fc5a5a7c0e3da53134f05b2103e1998a304aa9
do-Va <varisdogukan@outlook.com>
2022-06-20 21:06:51 +0300

    updated createTask controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,16 @@
+const Task = require('../models/task');
+
 const getAllTasks = (req, res) => {
   res.send('all items from the file');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
